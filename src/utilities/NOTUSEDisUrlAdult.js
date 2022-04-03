316ee170e1f734f496895addac6f929a722c1f0b
jules marchand <friarobaz@gmail.com>
2022-04-03 17:52:34 +0200

    Delete NOTUSEDisUrlAdult.js

--- src/utilities/NOTUSEDisUrlAdult.js
+++
@@ -1,9 +0,0 @@
-import { getDayUrl } from "./days"
-
-export const isUrlAdult = (url, season) => {
-  const days = season.days
-  const ageGroups = season.ageGroups
-  const currentDay = days.filter((day) => getDayUrl(day) === url)[0]
-  let index = currentDay.ageGroupIndex
-  return ageGroups[index].min >= 18
-}
