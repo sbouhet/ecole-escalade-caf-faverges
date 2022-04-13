b80779676ddddea7658ec54bea5d2360ec56a314
jules marchand <friarobaz@gmail.com>
2022-04-13 10:40:00 +0200

    wait for update on botth public and private doc

--- src/routes/prive/mon-compte/[id].svelte
+++ src/routes/prive/mon-compte/[id].svelte
@@ -15,6 +15,12 @@
     let student, medicalCertificateLink, medicalCertificateTimestamp, medicalCertificateStatus
     const unsub = onSnapshot(doc(db, "students", urlId), async (doc) => {
         student = await getStudent(urlId)
+        console.log("Public document changed")
+        console.log(student)
+    })
+    const unsubPrivate = onSnapshot(doc(db, "students", urlId, "privateCol", "privateDoc"), async (doc) => {
+        student = await getStudent(urlId)
+        console.log("Private document changed")
         console.log(student)
     })
     $:if(student && student.private){
