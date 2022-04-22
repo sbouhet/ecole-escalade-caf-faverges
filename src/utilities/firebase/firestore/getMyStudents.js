b2359f10cccec1e42657c2f1b46071bb922c14f2
jules marchand <friarobaz@gmail.com>
2022-04-22 19:26:40 +0200

    Update getMyStudents.js

--- src/utilities/firebase/firestore/getMyStudents.js
+++ src/utilities/firebase/firestore/getMyStudents.js
@@ -1,38 +1,29 @@
 import { _getDoc } from "$firestore/basics"
 import { getAuth } from "firebase/auth"
 import { BError } from "berror"
+import { db } from "$utils/firebase/firebase"
+import { getDocs, query, where, collectionGroup } from "firebase/firestore"
 
 export const getMyStudents = async (season) => {
   try {
     if (!season) throw "No season"
     let myStudents = []
     //Get current user
-    let currentUser = getAuth().currentUser
+    let email = getAuth().currentUser.email
 
     //If no user, return empty array
-    if (!currentUser) return []
+    if (!email) return []
 
-    //Get document for current user
-    const userDoc = await _getDoc("users", currentUser.uid)
+    const students = query(
+      collectionGroup(db, "privateCol"),
+      where("emails", "array-contains", "email")
+    )
+    const querySnapshot = await getDocs(students)
+    querySnapshot.forEach((student) => {
+      myStudents.push(student.data())
+    })
 
-    //Get student IDs from document
-    const studentIds = userDoc.students
-
-    //For all IDs
-    for (const id of studentIds) {
-      //Get student document
-      const studentDoc = await _getDoc("students", id)
-
-      //Check ff season exists and status is not null
-      if (
-        studentDoc.seasons[season.name] &&
-        studentDoc.seasons[season.name].status
-      ) {
-        //Add document to MyStudents
-        myStudents.push(studentDoc)
-      }
-    }
-    //console.log(`Found ${myStudents.length} student for ${currentUser.email}`)
+    console.log(`Found ${myStudents.length} student for ${email}`)
     return myStudents
   } catch (error) {
     throw new BError("$firestore/getMyStudents => getMyStudents()", error)
