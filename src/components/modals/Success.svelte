979aed594c4c9cd51dbd6a9552d0d260a1e2ce0d
friarobaz <friarobaz@gmail.com>
2022-07-08 14:24:14 +0200

    warning page on success

--- src/components/modals/Success.svelte
+++ src/components/modals/Success.svelte
@@ -16,19 +16,35 @@
       $subscriptionStatus = null
       $goto('/prive/mon-compte')
     }
+
+  
 </script>
 
 <dialog open={open}>
   <article>
     <h2>Votre demande d'inscription a bien été enregistrée !</h2>
-    <p><b>{printName($subscription.publicInfo)}</b> est pré‑inscrit·e sur le créneau de {getDayName($currentDay)}</p>
-    <p>Accédez à votre compte pour connaitre les prochaines étapes.</p>
-      
+    <p><b>{printName($subscription.publicInfo)}</b> est pré‑inscrit·e sur le créneau de {getDayName($currentDay)}.</p>
+    <div>
+      <strong style="color:red"> Attention !</strong><br>
+      Il reste plusieurs étapes pour finaliser votre inscription :
+    </div>
+    <br>
+      <ul>
+        <li>Payer les cours</li>
+        <li>Prendre une licence au CAF de Faverges</li>
+        <li>Transférer un certificat médical</li>
+      </ul>
     
+    <div>
+      Vous avez jusqu'au <b>{dayjs($currentSeason.deadline).format("dddd D MMMM à HH:mm")}</b> pour finaliser votre inscription.
+      <span style="color:red">Après cette date, votre place sera libérée et proposée aux personnes inscrites sur la liste d'attente.</span>
+    </div>
+    <br>
+    <p>Accédez à votre compte pour finaliser l'inscription.</p>
       
     <footer>
       <div role="button" class="outline" on:click={subscribeAnother}>Inscire quelqu'un d'autre</div>
-      <div role="button" on:click={done}>Continuer</div>
+      <div role="button" on:click={done}>Finaliser l'inscription</div>
     </footer>
   </article>
 </dialog>
