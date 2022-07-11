9e60a53f5ae1eb7345962775df3c492f053ee7dd
friarobaz <friarobaz@gmail.com>
2022-07-11 08:51:35 +0200

    stuff

--- src/routes/prive/mon-compte/[id]/index.svelte
+++ src/routes/prive/mon-compte/[id]/index.svelte
@@ -3,22 +3,20 @@
     import {params} from '@roxi/routify'
     import { getStudent } from '$firestore/getStudent'
     import { printName } from '$utils/printName'
-    import ErrorMessage from '$components/htmlElements/ErrorMessage.svelte'
     import { currentSeason } from '$utils/stores'
     import { translate } from '$utils/TRANSLATE'
     import Boolean from '$components/htmlElements/Boolean.svelte'
     import MedicalCertificate from '$components/subscriptionSteps/MedicalCertificate.svelte'
     import HelloAsso from '$components/subscriptionSteps/HelloAsso.svelte'
     import Licence from '$components/subscriptionSteps/Licence.svelte'
-    import { doc, onSnapshot } from "firebase/firestore";
+    import { doc, onSnapshot } from "firebase/firestore"
     import { db } from "$utils/firebase/firebase"
-    import { getAgeGroupFromDayUrl } from '$utils/ageGroups'
-    import { goto } from '@roxi/routify'
-    import Back from '$components/htmlElements/Back.svelte'
+    import {capitalize} from '$utils/capitalize'
+  
 
     let error
     let urlId = $params.id
-    let student, medicalCertificateLink, medicalCertificateTimestamp, medicalCertificateStatus, firstName, lastName, paymentStatus, slug
+    let student
     const unsub = onSnapshot(doc(db, "students", urlId), async (doc) => {
         try {
             student = await getStudent(urlId)
@@ -42,23 +40,29 @@
                 <h1>{printName(student.public)}</h1>
                 <h4>{translate(student.public.seasons[$currentSeason.name].status)}</h4>
             </hgroup>
-            <small>Cliquez sur chaque étape pour plus de détails.</small>
-            <br><br>
-    
-            
-            <details>
-                <summary role="button" class="outline"><Boolean value={'yes'} big={true}/>Étape 1 : Créer un compte sur le site de l'école d'escalade</summary>
-                <p style="color:green">Votre compte a bien été créé !</p>
-            </details>
-            <HelloAsso {student} open={$params.payment}/>
-            <Licence {student} open={$params.licence}/>
-            <MedicalCertificate  {student} open={$params.certificate}/>
+            {#if student.public.seasons[$currentSeason.name].payment==="yes" && student.public.seasons[$currentSeason.name].licence==="yes" && student.public.seasons[$currentSeason.name].medicalCertificate==="yes"}
+                <strong style="color:green">L'inscription est terminée !</strong><br>
+                <div style="color:green">{capitalize(student.public.firstName)} est bien inscrit·e aux cours d'escalade du CAF de Faverges pour la saison {$currentSeason.name}</div>
+                <br><br><br>
+            {:else}
+                <small>Cliquez sur chaque étape pour plus de détails.</small>
+                <br><br>
+        
+                
+                <details>
+                    <summary role="button" class="outline"><Boolean value={'yes'} big={true}/>Étape 1 : Créer un compte sur le site de l'école d'escalade</summary>
+                    <p style="color:green">Votre compte a bien été créé !</p>
+                </details>
+                <HelloAsso {student} open={$params.payment}/>
+                <Licence {student} open={$params.licence}/>
+                <MedicalCertificate  {student} open={$params.certificate}/>
+            {/if}
         </article>
     {:else}
         <div style="color:red">Erreur : cet élève n'est pas inscrit sur la saison {$currentSeason.name}</div>
     {/if}
 {:else}
-    <div style="color:red">Erreur : cet élève n'existe pas</div>
+    <div style="color:red">Erreur : cet élève n'existe pas ou vous n'avez pas les droits pour le voir</div>
 {/if}
 
 <slot></slot>
