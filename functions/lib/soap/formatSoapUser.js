f492b3e0da4f2ff9bb1818181a532672565e0a14
friarobaz <friarobaz@gmail.com>
2022-09-01 22:09:37 +0200

    fix licence bug

--- functions/lib/soap/formatSoapUser.js
+++ functions/lib/soap/formatSoapUser.js
@@ -1,9 +1,17 @@
 module.exports = (soapUser) => {
-  const usr = soapUser.verifierUnAdherentReturn
-  return {
-    exists: usr.existe.$value === 1,
-    signupDate: usr.inscription.$value,
-    lastName: usr.nom.$value,
-    firstName: usr.prenom.$value,
+  try {
+    //const usr = soapUser.verifierUnAdherentReturn
+    const usr = soapUser.return
+    //console.log('TEST TEST TEST');
+    //console.log(JSON.stringify(usr))
+    return {
+      exists: usr.existe.$value === 1,
+      signupDate: usr.inscription.$value,
+      lastName: usr.nom.$value,
+      firstName: usr.prenom.$value,
+    }
+  } catch (error) {
+    console.error("Error formating soap user")
+    console.error(error);
   }
 }
--- functions/lib/soap/getFormattedSoapUser.js
+++ functions/lib/soap/getFormattedSoapUser.js
@@ -8,7 +8,9 @@ module.exports = async (userId) => {
     const client = await getClient()
     const auth = await getAuth(client)
     const user = await getSoapUser(client, auth, userId)
+    console.log(user);
     const formatted = formatSoapUser(user)
+    console.log(formatted);
     return formatted
   } catch (error) {
     throw "Utilisateur non trouvé sur la base de donnée du CAF"
