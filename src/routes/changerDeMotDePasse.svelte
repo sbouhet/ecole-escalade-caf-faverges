3eac98d81a1606fb98d4179d43f6a67a12dcbcef
friarobaz <friarobaz@gmail.com>
2022-09-02 13:56:32 +0200

    better change password

--- src/routes/changerDeMotDePasse.svelte
+++ src/routes/changerDeMotDePasse.svelte
@@ -4,17 +4,25 @@
     import ErrorMessage from '$components/htmlElements/ErrorMessage.svelte'
     let email = $params.email
     let error = null
+    let loading, success
+    let emailSent
 
     const handleClick = ()=>{
+        if (loading) return
+        loading = true
+        success = false
+        error = null
         sendPasswordResetEmail(getAuth(), email)
         .then(() => {
-            console.log('succes');
+            emailSent = email
+            console.log('succes')
+            success = true
+            loading = false
         })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode);
-            console.log(errorMessage);
+        .catch((e) => {
+            loading = false
+            success = false
+            error = e
         });
     }
 </script>
@@ -25,8 +33,13 @@
         <label for="email">Adresse email</label>
         <input type="email" id="email" name="email" placeholder="Adresse email"  bind:value={email} required>
         <br>
-        <button on:click|preventDefault={handleClick}>Envoyer l'email de réinitialisation</button>
+        <button on:click|preventDefault={handleClick} disabled={loading} aria-busy={loading}>Envoyer l'email de réinitialisation</button>
         <ErrorMessage error={error}/>
+        {#if success}
+            <div style="color:green">
+                Un email a été envoyé à l'adresse {emailSent} pour réinitialiser votre mot de passe. Veuillez consulter votre messagerie.
+            </div>
+        {/if}
     </form>
 </div>
 
