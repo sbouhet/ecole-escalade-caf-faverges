3c506acc3d6f91b7882b19cbf8b2429f17bf13e1
friarobaz <friarobaz@gmail.com>
2023-07-02 18:36:16 +0200

    change june to august

--- src/utilities/seasons.js
+++ src/utilities/seasons.js
@@ -13,10 +13,14 @@ export const seasons = () => {
       }
     }
     const currentMonth = parseInt(dayjs().format("M"))
-    const juneOrLater = currentMonth > 5
+    //changed on 02/07/2023
+    //const juneOrLater = currentMonth > 5
+    const augustOrLater = currentMonth > 7
     const thisYear = parseInt(dayjs().format("YYYY"))
 
-    if (juneOrLater) return seasonObject(thisYear)
+    //changed on 02/07/2023
+    //if (juneOrLater) return seasonObject(thisYear)
+    if (augustOrLater) return seasonObject(thisYear)
     return seasonObject(thisYear - 1)
   } catch (error) {
     throw new BError("$utils/seasons => seasons()", error)
