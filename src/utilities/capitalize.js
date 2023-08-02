4e718764a1cd784f6b69439a4481da839e32d41c
friarobaz <friarobaz@gmail.com>
2023-08-02 17:12:17 +0200

    fix no string error

--- src/utilities/capitalize.js
+++ src/utilities/capitalize.js
@@ -2,7 +2,7 @@ import { BError } from "berror"
 
 export const capitalize = (string) => {
   try {
-    if (!string) throw "No string"
+    if (!string) return "????"
     string = string.toLowerCase()
     return string.charAt(0).toUpperCase() + string.slice(1)
   } catch (error) {
